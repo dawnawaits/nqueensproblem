4300cd747e8cf4d935a8741e74f5003491310882
dawnawaits <25034491+dawnawaits@users.noreply.github.com>
2020-06-04 22:51:23 +0530

    Update main.dart
    Removed error messages and optimised it slightly

--- main.dart
+++ main.dart
@@ -1,11 +1,9 @@
 class NQueens {
   NQueens(this.n) {
-    this._tempBoard = List<int>.generate(n + 1, (i) => -1);
     this.board = List<int>.generate(n + 1, (i) => -1);
   }
   final int n;
   List<int> board;
-  List<int> _tempBoard;
 
   void printBoard() {
     List<List<int>> displayBoard = List<List<int>>.generate(n, (i) {
@@ -25,37 +23,32 @@ class NQueens {
       bool isIntialized = (board[i] != -1);
       bool rightDiag = isIntialized ? (x + y) == (i + board[i]) : false;
       bool leftDiag =
-          isIntialized ? (x - y) == (i - _tempBoard[i]) : false;
-//       print("$x,$y, i: $i, board[i]: ${_tempBoard[i]}, LeftDiag: $leftDiag, RightDiag: $rightDiag");
-      if (_tempBoard[i] == y || leftDiag || rightDiag) {
+          isIntialized ? (x - y) == (i - board[i]) : false;
+      if (board[i] == y || leftDiag || rightDiag) {
         return false;
       }
     }
     return true;
   }
 
-  void solve([int row = 1]) {
+  bool solve([int row = 1]) {
     if (row == n + 1) {
-      board = List<int>.from(_tempBoard);
-//       print(board);
-      return;
+      return true;
     }
     for (int col = 1; col <= n; col++) {
       if (_canPlace(row, col)) {
-        _tempBoard[row] = col;
-//         print("$row, $col");
-        solve(row + 1);
+        board[row] = col;
+        if(solve(row + 1))
+          return true;
       }
-//       print("False: $row, $col");
-      _tempBoard[row] = -1;
+      board[row] = -1;
     }
-//     solve(row+1);
+    return false;
   }
 }
 
 void main() {
   NQueens q = NQueens(4);
   q.solve();
-//   print(q.board);
   q.printBoard();
 }
